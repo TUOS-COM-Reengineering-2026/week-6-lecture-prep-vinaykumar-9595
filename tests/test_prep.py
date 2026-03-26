a9b34cf09a622d402a97e6933377d8d71d957807
vinaykumar-9595 <vkbattula1@sheffield.ac.uk>
2026-03-26 07:39:56 +0000

    test_prep.py

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -4,8 +4,36 @@ from prep import strange_function
 class MyTestCase(unittest.TestCase):
     def test_strange_function1(self):
         self.assertEqual(
-            first=strange_function(1, 2, 3, 4),
-            second='behaviour 3'
+            first=strange_function(1, 1, 1, 2),
+            second='behaviour 1'
         )
 
-    # TODO: Can you write more test cases below to increase the test coverage of `strange_function`?
+    def test_strange_function2(self):
+        self.assertEqual(
+            first=strange_function(1, 1, 2, 1),
+            second='behaviour 2'
+        )
+
+    def test_strange_function3(self):
+        self.assertEqual(
+            first=strange_function(1, 2, 2, 1),
+            second='behaviour 3'
+        )
+    
+    def test_strange_function4(self):
+        self.assertEqual(
+            first=strange_function(1, 2, 1, 1),
+            second='behaviour 4'
+        )
+
+    def test_strange_function5(self):
+        self.assertEqual(
+            first=strange_function(3, 1, 1, 2),
+            second='behaviour 5'
+        )
+    
+    def test_strange_function6(self):
+        self.assertEqual(
+            first=strange_function(1, 2, 1, 3),
+            second='behaviour 6'
+        )
